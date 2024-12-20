b98c3ba2f7783b955d2694eb971a8ac39b3b9cda
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-12-20 22:02:17 +0900

    Feat : API 명세서 마이페이지, 내가 작성한 게시글 목록, 내가 작성한 리뷰 목록, 사용자 수정 추가

---
+++ src/users/dto/get-my-review-list.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GetMyReviewListDto {
+  @ApiProperty({
+    description: '리뷰 ID',
+  })
+  reviewId: number;
+
+  @ApiProperty({
+    description: '장소의 ID',
+  })
+  placeId: number;
+
+  @ApiProperty({
+    description: '리뷰 제목',
+  })
+  title: string;
+
+  @ApiProperty({
+    description: '리뷰 내용',
+  })
+  content: string;
+
+  @ApiProperty({
+    description: '사용자가 리뷰 좋아요를 클릭 했는지 여부',
+  })
+  isLike: boolean;
+}
