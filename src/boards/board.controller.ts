e9d3144c61ab81d7b93057912c1b46eb8dd22aa4
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-12-20 22:40:35 +0900

    Feat : API 명세서 게시글 조회 추가

--- src/boards/board.controller.ts
+++ src/boards/board.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { BoardService } from './board.service';
+import { GetBoardListResponseDto } from './dto/get-baord-list.dto';
 import { GetLatestListResponseDto } from './dto/get-latest-list-response.dto';
 import { GetPopularListResponseDto } from './dto/get-popular-list.dto';
 import { IBoardService } from './interface/board.service.interface';
@@ -43,4 +44,23 @@ export class BoardController {
   })
   @Get('latest-lists')
   async getLatestList(@Query() count: number) {}
+
+  @ApiOperation({
+    summary: '게시글 목록 조회',
+    description: `
+    - 등록된 게시글을 날짜를 내림차순으로 정렬하여 조회 합니다.
+    - 카테고리를 선택할 수 있습니다.`,
+  })
+  @ApiQuery({
+    name: 'category',
+    description: `
+    - 게시글의 카테고리
+    - 기본값 : 전체`,
+    required: false,
+  })
+  @ApiOkResponse({
+    type: [GetBoardListResponseDto],
+  })
+  @Get()
+  async getBoardList(@Query() category: string) {}
 }
