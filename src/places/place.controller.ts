f5be1363251e1ad19530a7b1feed473ae1a5e274
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-12-22 18:26:42 +0900

    Docs : Api 명세서 시설물 조회 추가

--- src/places/place.controller.ts
+++ src/places/place.controller.ts
@@ -1,22 +1,29 @@
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Inject,
-} from '@nestjs/common';
-import { PlaceService } from './place.service';
-import { CreatePlaceDto } from './dto/create-place.dto';
-import { UpdatePlaceDto } from './dto/update-place.dto';
+  GetNearbyPlaceListQueryDto,
+  GetNearbyPlaceListResponseDto,
+} from './dto/get-nearby-place-list.dto';
 import { IPlaceService } from './interface/place.service.interface';
+import { PlaceService } from './place.service';
 
 @Controller('places')
 export class PlaceController {
   constructor(
     @Inject(PlaceService)
     private readonly placeService: IPlaceService,
+  ) {}
+
+  @ApiOperation({
+    summary: '시설물의 위치 정보를 조회 합니다.',
+    description: `
+    - 입력받은 현재위치와 카테고리 그리고 반경을 기준으로 조회 합니다.`,
+  })
+  @ApiOkResponse({
+    type: GetNearbyPlaceListResponseDto,
+  })
+  @Get('nearby-place-lists')
+  async getNearbyPlaceList(
+    @Query() getNearbyPlaceListQueryDto: GetNearbyPlaceListQueryDto,
   ) {}
 }
