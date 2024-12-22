17b7eddd63899500fabbcb61e2531a963344b593
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-12-22 20:51:06 +0900

    Docs : Api 명세서 댓글 삭제 추가

--- src/comments/comment.controller.ts
+++ src/comments/comment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Param, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Inject, Param, Put } from '@nestjs/common';
 import { ApiOperation, ApiParam } from '@nestjs/swagger';
 import { CommentService } from './comment.service';
 import { UpdateOneDto } from './dto/update-one.dto';
@@ -26,4 +26,17 @@ export class CommentController {
     @Param('id') id: number,
     @Body() updateOneDto: UpdateOneDto,
   ) {}
+
+  @ApiOperation({
+    summary: '댓글을 삭제 합니다.',
+    description: `
+    - 댓글의 ID를 입력 받아 삭제 합니다.
+    `,
+  })
+  @ApiParam({
+    name: 'id',
+    description: '댓글의 ID',
+  })
+  @Delete(':id')
+  async deleteOne(@Param('id') id: number) {}
 }
