128a8064f7ba6df0ff689058ccd344dbe1c35e24
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-12-24 21:32:15 +0900

    Feat : exports UserService 추가

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,12 +1,13 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmCustomModule } from "src/common/typeorm/typeorm-custom.module";
-import { UserRepository } from "./user.repository";
-import { UserController } from "./user.controller";
-import { UserService } from "./user.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmCustomModule } from 'src/common/typeorm/typeorm-custom.module';
+import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmCustomModule.forCustomRepository([UserRepository])],
-    controllers: [UserController],
-    providers: [UserService]
+  imports: [TypeOrmCustomModule.forCustomRepository([UserRepository])],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
