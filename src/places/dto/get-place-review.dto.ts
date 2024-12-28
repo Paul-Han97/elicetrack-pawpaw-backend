67f3b5fc840500b0257ad3824f1fb0b26aa6b0fa
SETO <yesthis9090@gmail.com>
2024-12-28 22:03:38 +0900

    Feat : GetPlaceReviewDto 추가

--- src/places/dto/get-place-review.dto.ts
+++ src/places/dto/get-place-review.dto.ts
@@ -2,6 +2,38 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class GetPlaceReviewDto {
   @ApiProperty({
+    description: '리뷰가 작정된 장소의 ID',
+  })
+  id: number;
+
+  @ApiProperty({
+    description: '유저의 ID',
+  })
+  userId: number;
+
+  @ApiProperty({
+    description: '리뷰의 ID',
+  })
+  reviewId: number;
+}
+
+export class GetPlaceReviewResponseDto {
+  @ApiProperty({
+    description: '리뷰의 ID',
+  })
+  reviewId: number;
+
+  @ApiProperty({
+    description: '리뷰 작성자의 정보',
+  })
+  author: { id: number; imageUrl: string };
+
+  @ApiProperty({
+    description: '리뷰가 작성된 장소의 ID',
+  })
+  id: number;
+
+  @ApiProperty({
     description: '리뷰의 제목',
   })
   title: string;
@@ -9,10 +41,10 @@ export class GetPlaceReviewDto {
   @ApiProperty({
     description: '리뷰의 내용',
   })
-  conent: string;
+  content: string;
 
   @ApiProperty({
-    description: '좋아요 선택 여부',
+    description: '리뷰의 좋아요 상태',
   })
   isLikeClicked: boolean;
 
