38feb480af431bddfba40197f9d1fe720bd2f3d7
SETO <yesthis9090@gmail.com>
2024-12-29 19:36:16 +0900

    Update : getPlaceReview, updateReview 추가

--- src/places/interface/place.service.interface.ts
+++ src/places/interface/place.service.interface.ts
@@ -1,6 +1,8 @@
 import { ResponseData } from 'src/common/types/response.type';
 import { CreatePlaceReviewDto } from '../dto/create-place-review.dto';
 import { GetPlaceResponseDto } from '../dto/get-place.dto';
+import { GetPlaceReviewDto } from '../dto/get-place-review.dto';
+import { UpdatePlaceReviewDto } from '../dto/update-place-review.dto';
 
 export interface IPlaceService {
   saveEntities(): Promise<void>;
@@ -16,4 +18,8 @@ export interface IPlaceService {
   createPlaceReview(
     createPlaceReviewDto: CreatePlaceReviewDto,
   ): Promise<ResponseData>;
+
+  getPlaceReview(getPlaceReviewDto: GetPlaceReviewDto): Promise<ResponseData>;
+
+  updateReview(updatePlaceReviewDto: UpdatePlaceReviewDto);
 }
