5fca5892225fe0fe9cdf9207cd23238e75c491c1
SETO <yesthis9090@gmail.com>
2024-12-29 19:35:13 +0900

    Update : UpdatePlaceReviewDto 추가

--- src/places/dto/update-place-review.dto.ts
+++ src/places/dto/update-place-review.dto.ts
@@ -9,12 +9,27 @@ export class UpdatePlaceReviewDto {
   @ApiProperty({
     description: '리뷰의 내용',
   })
-  conent: string;
+  content: string;
 
   @ApiProperty({
     description: '좋아요 선택 여부',
   })
   isLikeClicked: boolean;
+
+  @ApiProperty({
+    description: '장소의 ID',
+  })
+  id: number;
+
+  @ApiProperty({
+    description: '유저의 ID',
+  })
+  userId: number;
+
+  @ApiProperty({
+    description: '리뷰의 ID',
+  })
+  reviewId: number;
 }
 
 export class UpdatePlaceReviewResponseDto {
