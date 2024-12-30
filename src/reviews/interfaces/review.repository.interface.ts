0ab21f0812ec2af195de83d122dbc4a519d619a7
SETO <yesthis9090@gmail.com>
2024-12-30 20:42:11 +0900

    Feat : findMyReviewList 추상화 추가

--- src/reviews/interfaces/review.repository.interface.ts
+++ src/reviews/interfaces/review.repository.interface.ts
@@ -1,4 +1,10 @@
 import { Repository } from 'typeorm';
 import { Review } from '../entities/review.entity';
 
-export interface IReviewRepository extends Repository<Review> {}
+export interface IReviewRepository extends Repository<Review> {
+    findMyReviewList(
+    userId: number,
+    take: number,
+    skip: number,
+  ): Promise<[Review[], number]>;
+}
