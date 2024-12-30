8c79aef82149e7fe833d4b0eafe61cc69119306e
SETO <yesthis9090@gmail.com>
2024-12-30 15:44:37 +0900

    Update : checkDuplicateNickname추가

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,7 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { UserRepository } from './user.repository';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { ERROR_MESSAGE } from 'src/common/constants';
+import { User } from './entities/user.entity';
 import { IUserRepository } from './interfaces/user.repository.interface';
 import { IUserService } from './interfaces/user.service.interface';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService implements IUserService {
@@ -9,4 +11,17 @@ export class UserService implements IUserService {
     @Inject(UserRepository)
     private readonly userRepository: IUserRepository,
   ) {}
+
+  async checkDuplicateNickname(nickname: string): Promise<User | undefined> {
+    if (!nickname || nickname.trim() === '') {
+      throw new BadRequestException('닉네임은 필수 입력값입니다.');
+    }
+    const user = await this.userRepository.findByNickName(nickname);
+
+    if (user) {
+      throw new BadRequestException(ERROR_MESSAGE.NICKNAME_ALREADY_EXIST);
+    }
+
+    return;
+  }
 }
