00f7a5e3a3bb101f165a660c826406d97320d30c
SETO <yesthis9090@gmail.com>
2024-12-30 16:14:45 +0900

    Update : reviewRepository 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,12 @@ import { UserRepository } from 'src/users/user.repository';
 import { UserService } from 'src/users/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ReviewRepository } from 'src/reviews/review.repository';
 
 @Module({
-  imports: [TypeOrmCustomModule.forCustomRepository([UserRepository])],
+  imports: [
+    TypeOrmCustomModule.forCustomRepository([UserRepository, ReviewRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService],
 })
