262a6aad8021addce4cac1849a0cd5eded76198b
SETO <yesthis9090@gmail.com>
2024-12-30 15:43:20 +0900

    Update : findByNickName 추가

--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -2,6 +2,7 @@ import { CustomRepository } from 'src/common/typeorm/typeorm-custom.decorator';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { IUserRepository } from './interfaces/user.repository.interface';
+import { GetMyReviewListDto } from './dto/get-my-review-list.dto';
 
 @CustomRepository(User)
 export class UserRepository
@@ -10,11 +11,24 @@ export class UserRepository
 {
   async findUserCredentialByEmail(username: string): Promise<User> {
     const reuslt = await this.createQueryBuilder('user')
-      .select(['user.id', 'user.canWalkingMate','credential.username', 'credential.password', 'role.type'])
+      .select([
+        'user.id',
+        'user.canWalkingMate',
+        'credential.username',
+        'credential.password',
+        'role.type',
+      ])
       .leftJoinAndSelect('user.credential', 'credential')
       .leftJoinAndSelect('user.role', 'role')
       .where('credential.username = :username', { username })
       .getOne();
     return reuslt;
+  }
+
+  async findByNickName(nickname: string): Promise<User | undefined> {
+    const result = await this.createQueryBuilder('user')
+      .where('user.nickname = :nickname', { nickname })
+      .getOne();
+    return result;
   }
 }
