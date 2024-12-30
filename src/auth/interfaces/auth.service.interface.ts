8a95d591e7826d416a7a57e4dd24f5a5ef5264ca
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-12-31 05:05:42 +0900

    Feat : 회원가입 기능 추가

--- src/auth/interfaces/auth.service.interface.ts
+++ src/auth/interfaces/auth.service.interface.ts
@@ -1,12 +1,20 @@
 import { ResponseData } from 'src/common/types/response.type';
 import { LoginDto, LoginResponseDto } from '../dto/login.dto';
+import { RegisterDto } from '../dto/register.dto';
+import { SendTemporaryPasswordEmailDto } from '../dto/send-temporary-password-email.dto';
 import { SendVerificationEmailDto } from '../dto/send-verification-email.dto';
 import { ValidateVerificationDto } from '../dto/validate-verifcation-code.dto';
-import { SendTemporaryPasswordEmailDto } from '../dto/send-temporary-password-email.dto';
 
 export interface IAuthService {
   login(loginDto: LoginDto): Promise<ResponseData<LoginResponseDto>>;
-  sendVerificationEmail(sendVerificationEmailDto: SendVerificationEmailDto): Promise<ResponseData>
-  validateVerificationCode(validateVerificationCodeDto: ValidateVerificationDto): Promise<ResponseData>
-  sendTemporaryPasswordEmail(sendTemporaryPasswordEmailDto: SendTemporaryPasswordEmailDto): Promise<ResponseData>
+  register(registerDto: RegisterDto): Promise<ResponseData>;
+  sendVerificationEmail(
+    sendVerificationEmailDto: SendVerificationEmailDto,
+  ): Promise<ResponseData>;
+  validateVerificationCode(
+    validateVerificationCodeDto: ValidateVerificationDto,
+  ): Promise<ResponseData>;
+  sendTemporaryPasswordEmail(
+    sendTemporaryPasswordEmailDto: SendTemporaryPasswordEmailDto,
+  ): Promise<ResponseData>;
 }
