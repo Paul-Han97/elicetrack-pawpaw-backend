30de6747aa674645e468c4f4657508566c807097
SETO <yesthis9090@gmail.com>
2024-12-30 17:06:23 +0900

    Update : duplicateNicknameQueryDto 매개변수 추가

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
-import { ERROR_MESSAGE } from 'src/common/constants';
-import { User } from './entities/user.entity';
+import { ERROR_MESSAGE, SUCCESS_MESSAGE } from 'src/common/constants';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ResponseData } from 'src/common/types/response.type';
+import { IReviewRepository } from 'src/reviews/interfaces/review.repository.interface';
+import { ReviewRepository } from 'src/reviews/review.repository';
+import { DuplicateNicknameQueryDto } from './dto/duplicate-nickname.dto';
 import { IUserRepository } from './interfaces/user.repository.interface';
 import { IUserService } from './interfaces/user.service.interface';
 import { UserRepository } from './user.repository';
-import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { ReviewRepository } from 'src/reviews/review.repository';
-import { IReviewRepository } from 'src/reviews/interfaces/review.repository.interface';
 
 @Injectable()
 export class UserService implements IUserService {
@@ -17,17 +18,23 @@ export class UserService implements IUserService {
     private readonly reviewRepository: IReviewRepository,
   ) {}
 
-  async checkDuplicateNickname(nickname: string): Promise<User | undefined> {
-    if (!nickname || nickname.trim() === '') {
-      throw new BadRequestException('닉네임은 필수 입력값입니다.');
-    }
-    const user = await this.userRepository.findByNickName(nickname);
+  async checkDuplicateNickname(
+    duplicateNicknameQueryDto: DuplicateNicknameQueryDto,
+  ): Promise<ResponseData> {
+    const user = await this.userRepository.findByNickName(
+      duplicateNicknameQueryDto,
+    );
 
     if (user) {
       throw new BadRequestException(ERROR_MESSAGE.NICKNAME_ALREADY_EXIST);
     }
 
-    return;
+    const resData: ResponseData = {
+      message: SUCCESS_MESSAGE.REQUEST,
+      data: null,
+    };
+
+    return resData;
   }
 
   async getMyReviewList(paginationDto: PaginationDto) {}
