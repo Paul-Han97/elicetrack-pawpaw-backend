2df1e5a1bb2f96a78ee106646596df4bda479d39
SETO <yesthis9090@gmail.com>
2024-12-30 15:48:25 +0900

    Update : findByNickName 추가

--- src/users/interfaces/user.repository.interface.ts
+++ src/users/interfaces/user.repository.interface.ts
@@ -1,6 +1,9 @@
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { GetMyReviewListDto } from '../dto/get-my-review-list.dto';
 
 export interface IUserRepository extends Repository<User> {
-    findUserCredentialByEmail(username: string): Promise<User>
+  findUserCredentialByEmail(username: string): Promise<User>;
+
+  findByNickName(nickname: string): Promise<User | undefined>;
 }
