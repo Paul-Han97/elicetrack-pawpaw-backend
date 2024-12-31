2298256f580f7ed9f488d9a9626e680bf4e6814c
SETO <yesthis9090@gmail.com>
2024-12-31 14:23:55 +0900

    Feat : BoardRepository 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
+import { BoardRepository } from 'src/boards/board.repository';
 import { TypeOrmCustomModule } from 'src/common/typeorm/typeorm-custom.module';
+import { CredentialRepository } from 'src/credentials/credential.repository';
+import { ImageRepository } from 'src/images/image.repository';
+import { ImageService } from 'src/images/image.service';
+import { ReviewRepository } from 'src/reviews/review.repository';
+import { UserImageRepository } from 'src/user-images/user-image.repository';
 import { UserRepository } from 'src/users/user.repository';
 import { UserService } from 'src/users/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { CredentialRepository } from 'src/credentials/credential.repository';
-import { ReviewRepository } from 'src/reviews/review.repository';
-import { ImageRepository } from 'src/images/image.repository';
-import { UserImageRepository } from 'src/user-images/user-image.repository';
-import { ImageService } from 'src/images/image.service';
 
 @Module({
   imports: [
-    TypeOrmCustomModule.forCustomRepository([UserRepository, CredentialRepository, ReviewRepository, ImageRepository, UserImageRepository]),
+    TypeOrmCustomModule.forCustomRepository([
+      UserRepository,
+      CredentialRepository,
+      ReviewRepository,
+      ImageRepository,
+      UserImageRepository,
+      BoardRepository,
+    ]),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserService, ImageService],
