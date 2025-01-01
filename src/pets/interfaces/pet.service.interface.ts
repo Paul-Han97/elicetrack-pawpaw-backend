ad61f0c57d204974eea5ef005cf0ea89d5f93464
SETO <yesthis9090@gmail.com>
2025-01-01 23:47:42 +0900

    Feat : creatPet 추상화 생성

--- src/pets/interfaces/pet.service.interface.ts
+++ src/pets/interfaces/pet.service.interface.ts
@@ -1,1 +1,10 @@
-export interface IPetService {}
+import { ResponseData } from 'src/common/types/response.type';
+import { CreatePetDto, CreatePetResponseDto } from '../dto/create-pet.dto';
+
+export interface IPetService {
+  createPet(
+    createPetDto: CreatePetDto,
+    userId: number,
+    image?: Express.Multer.File,
+  ): Promise<ResponseData<CreatePetResponseDto>>;
+}
