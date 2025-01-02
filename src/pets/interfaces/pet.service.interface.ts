b1b23a284b9d170135c9c41c3b4720b887fab0fa
SETO <yesthis9090@gmail.com>
2025-01-02 16:06:51 +0900

    Update : 매개변수 통합 및 변경

--- src/pets/interfaces/pet.service.interface.ts
+++ src/pets/interfaces/pet.service.interface.ts
@@ -4,7 +4,6 @@ import { CreatePetDto, CreatePetResponseDto } from '../dto/create-pet.dto';
 export interface IPetService {
   createPet(
     createPetDto: CreatePetDto,
-    userId: number,
     image?: Express.Multer.File,
   ): Promise<ResponseData<CreatePetResponseDto>>;
 }
