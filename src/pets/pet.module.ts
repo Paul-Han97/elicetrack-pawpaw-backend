b96d983afd87db98c65d7c955bd5972f762f3653
SETO <yesthis9090@gmail.com>
2025-01-02 16:05:02 +0900

    Update : gender, petsize, image, petimage repository추가

--- src/pets/pet.module.ts
+++ src/pets/pet.module.ts
@@ -1,12 +1,25 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmCustomModule } from "src/common/typeorm/typeorm-custom.module";
-import { PetRepository } from "./pet.repository";
-import { PetController } from "./pet.controller";
-import { PetService } from "./pet.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmCustomModule } from 'src/common/typeorm/typeorm-custom.module';
+import { GenderRepository } from 'src/genders/gender.repository';
+import { PetSizeRepository } from 'src/pet-sizes/pet-size..repository';
+import { PetController } from './pet.controller';
+import { PetRepository } from './pet.repository';
+import { PetService } from './pet.service';
+import { ImageRepository } from 'src/images/image.repository';
+import { PetImageRepository } from 'src/pet-images/pet-image.repository';
+import { ImageService } from 'src/images/image.service';
 
 @Module({
-    imports: [TypeOrmCustomModule.forCustomRepository([PetRepository])],
-    controllers: [PetController],
-    providers: [PetService]
+  imports: [
+    TypeOrmCustomModule.forCustomRepository([
+      PetRepository,
+      GenderRepository,
+      PetSizeRepository,
+      ImageRepository,
+      PetImageRepository
+    ]),
+  ],
+  controllers: [PetController],
+  providers: [PetService, ImageService],
 })
 export class PetModule {}
