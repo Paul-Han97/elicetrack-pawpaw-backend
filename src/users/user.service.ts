f34609a187e3bb4050e6684af6eb86f886306108
SETO <yesthis9090@gmail.com>
2025-01-04 03:20:16 +0900

    Feat : 회원정보 수정(비밀번호 변경), UpdateUserResponseDto 추가,utilService주입

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -10,11 +10,21 @@ import { IBoardRepository } from 'src/boards/interface/board.repository.interfac
 import { ERROR_MESSAGE, SUCCESS_MESSAGE } from 'src/common/constants';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { ResponseData } from 'src/common/types/response.type';
+import { DeleteImageDto } from 'src/images/dto/delete-image.dto';
+import { UploadImageDto } from 'src/images/dto/upload-image.dto';
+import { Image } from 'src/images/entities/image.entity';
+import { ImageRepository } from 'src/images/image.repository';
+import { ImageService } from 'src/images/image.service';
+import { IImageRepository } from 'src/images/interface/image.repository.interface';
+import { IImageService } from 'src/images/interface/image.service.interface';
 import { Location } from 'src/locations/entities/location.entity';
 import { ILocationRepository } from 'src/locations/interface/location.repository.interface';
 import { LocationRepository } from 'src/locations/location.repository';
 import { IReviewRepository } from 'src/reviews/interfaces/review.repository.interface';
 import { ReviewRepository } from 'src/reviews/review.repository';
+import { UserImage } from 'src/user-images/entities/user-image.entity';
+import { IUserImageRepository } from 'src/user-images/interface/user-image.repository.interface';
+import { UserImageRepository } from 'src/user-images/user-image.repository';
 import { UserLocation } from 'src/user-locations/entities/user-location.entity';
 import { IUserLocationRepository } from 'src/user-locations/interface/user-location.repository.interface';
 import { UserLocationRepository } from 'src/user-locations/user-location.repository';
@@ -30,9 +40,15 @@ import {
   SaveUserLocationDto,
 } from './dto/get-nearby-user-list.dto';
 import { GetUserDto, GetUserResponseDto } from './dto/get-user.dto';
+import { UpdateUserDto, UpdateUserResponseDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { IUserRepository } from './interfaces/user.repository.interface';
 import { IUserService } from './interfaces/user.service.interface';
 import { UserRepository } from './user.repository';
+import { UtilService } from 'src/common/utils/util.service';
+import { PasswordManager } from 'src/common/utils/password-manager.util';
+import { CredentialRepository } from 'src/credentials/credential.repository';
+import { ICredentialRepository } from 'src/credentials/interfaces/credential.repository.interface';
 
 @Injectable()
 export class UserService implements IUserService {
@@ -43,12 +59,22 @@ export class UserService implements IUserService {
     private readonly reviewRepository: IReviewRepository,
     @Inject(BoardRepository)
     private readonly boardRepository: IBoardRepository,
+    @Inject(ImageService)
+    private readonly imageService: IImageService,
+    @Inject(ImageRepository)
+    private readonly imageRepository: IImageRepository,
+    @Inject(UserImageRepository)
+    private readonly userImageRepository: IUserImageRepository,
     @Inject(getDataSourceToken())
     private readonly dataSource: DataSource,
     @Inject(LocationRepository)
     private readonly locationRepository: ILocationRepository,
     @Inject(UserLocationRepository)
     private readonly userLocationRepository: IUserLocationRepository,
+    @Inject(UtilService)
+    private readonly utilService: UtilService,
+    @Inject(CredentialRepository)
+    private readonly credentialRepository: ICredentialRepository,
   ) {}
 
   async getUser(
@@ -317,5 +343,144 @@ export class UserService implements IUserService {
     };
 
     return resData;
+  }
+
+  async updateUser(
+    updateUserDto: UpdateUserDto,
+  ): Promise<ResponseData<UpdateUserResponseDto>> {
+    const { image, canWalkingMate, nickname, password, newPassword } =
+      updateUserDto;
+    const id = updateUserDto.id;
+    const tempDeleteImageDto = new DeleteImageDto();
+
+    try {
+      const result = await this.dataSource.transaction<UpdateUserResponseDto>(
+        async (manager: EntityManager): Promise<UpdateUserResponseDto> => {
+          const userRepository = manager.withRepository(this.userRepository);
+          const imageRepository = manager.withRepository(this.imageRepository);
+          const userImageRepository = manager.withRepository(
+            this.userImageRepository,
+          );
+          const credentialRepository = manager.withRepository(
+            this.credentialRepository,
+          );
+
+          const user = await userRepository
+            .createQueryBuilder('user')
+            .leftJoinAndSelect('user.userImage', 'userImage')
+            .leftJoinAndSelect('user.credential', 'credential')
+            .leftJoinAndSelect('userImage.image', 'image')
+            .where('user.id = :id', { id })
+            .getOne();
+
+          if (!user) {
+            throw new NotFoundException(ERROR_MESSAGE.NOT_FOUND);
+          }
+
+          const updateUser = new User();
+          updateUser.id = user.id;
+          updateUser.nickname = nickname || user.nickname;
+          updateUser.canWalkingMate =
+            String(canWalkingMate).toLowerCase() === 'true';
+
+          if (password && newPassword) {
+            const isPasswordValid =
+              await this.utilService.passwordManager.compare(
+                password,
+                user.credential[0].password,
+              );
+
+            if (!isPasswordValid) {
+              throw new BadRequestException(
+                ERROR_MESSAGE.EMAIL_PASSWORD_NOT_MATCH,
+              );
+            }
+
+            const hashedPassword =
+              await this.utilService.passwordManager.hash(newPassword);
+            user.credential[0].password = hashedPassword;
+
+            await credentialRepository.save(user.credential[0]);
+          }
+
+          if (!image) {
+            const images = user.userImage;
+            tempDeleteImageDto.filenameList = images
+              .filter((userImage) => userImage?.image?.url)
+              .map((userImage) => userImage.image.url.split('/').pop() || '');
+
+            if (tempDeleteImageDto.filenameList.length > 0) {
+              await this.imageService.deleteImageFromS3(tempDeleteImageDto);
+
+              for (const userImage of images) {
+                await userImageRepository.remove(userImage);
+                if (userImage.image) {
+                  await imageRepository.remove(userImage.image);
+                }
+              }
+            }
+          } else {
+            const images = user.userImage || [];
+            tempDeleteImageDto.filenameList = images
+              .filter((userImage) => userImage?.image?.url)
+              .map((userImage) => userImage.image.url.split('/').pop() || '');
+
+            if (tempDeleteImageDto.filenameList.length > 0) {
+              await this.imageService.deleteImageFromS3(tempDeleteImageDto);
+
+              for (const userImage of images) {
+                await userImageRepository.remove(userImage);
+                if (userImage.image) {
+                  await imageRepository.remove(userImage.image);
+                }
+              }
+            }
+
+            const uploadImageDto = new UploadImageDto();
+            uploadImageDto.entity.id = user.id;
+            uploadImageDto.entity.name = User.name;
+            uploadImageDto.imageList.push(image);
+
+            const uploadedImage =
+              await this.imageService.uploadImageToS3(uploadImageDto);
+            tempDeleteImageDto.filenameList.push(
+              uploadedImage.imageList[0].filename,
+            );
+
+            const newImage = new Image();
+            newImage.url = uploadedImage.imageList[0].url;
+            newImage.createdUser = user.id.toString();
+            newImage.updatedUser = user.id.toString();
+
+            const userImage = new UserImage();
+            userImage.user = updateUser;
+            userImage.image = newImage;
+            userImage.createdUser = user.id.toString();
+            userImage.updatedUser = user.id.toString();
+
+            await imageRepository.save(newImage);
+            await userImageRepository.save(userImage);
+          }
+
+          const updateUserResponseDto = new UpdateUserResponseDto();
+          updateUserResponseDto.id = user.id;
+
+          const savedUser = await userRepository.save(updateUser);
+          return {
+            id: savedUser.id,
+          };
+        },
+      );
+
+      return {
+        message: SUCCESS_MESSAGE.REQUEST,
+        data: result,
+      };
+    } catch (e) {
+      if (tempDeleteImageDto.filenameList.length > 0) {
+        await this.imageService.deleteImageFromS3(tempDeleteImageDto);
+      }
+      throw e;
+    }
   }
 }
