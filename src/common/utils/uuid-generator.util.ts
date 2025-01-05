b890117f045d7193dfcba1eccefd4dcd89cc71c9
[Node1] 한바울 <wtg.paulhan@gmail.com>
2025-01-05 17:20:10 +0900

    Update : uuid v4 generator를 nodejs 내장함수인 crypto로 변경

--- src/common/utils/uuid-generator.util.ts
+++ src/common/utils/uuid-generator.util.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UuidGenerator {
   generate(): string {
-    return uuidv4();
+    return crypto.randomUUID();
   }
 }
